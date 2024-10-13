fd7e843a4f2a70969c2e0220dacd49690d4a9670
247686 Kaczmarek Jakub <247686@edu.p.lodz.pl>
2024-10-13 11:35:43 +0200

    generic Repository performance peak es

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -2,12 +2,15 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.*;
+
+import java.util.List;
 
 public class Repository {
     private static EntityManagerFactory emf;
 
     private static void emfChecker(){
-        if (emf == null){
+        if (emf == null) {
             emf = Persistence.createEntityManagerFactory("nbd");
         }
     }
@@ -27,9 +30,46 @@ public class Repository {
         } finally {
             em.close();
         }
-
-
     }
-
-
+    private static <T> List<T> getAll(Class<T> objClass){
+        emfChecker();
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<T> query = cb.createQuery(objClass);
+        Root<T> rootEntry = query.from(objClass);
+        query.select(rootEntry);
+        return em.createQuery(query).getResultList();
+    }
+    private static <T> List<T> getBy(Class<T> objClass, Object parameter, String parameterName){
+        emfChecker();
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<T> query = cb.createQuery(objClass);
+        Root<T> rootEntry = query.from(objClass);
+        Predicate predicate = cb.equal(rootEntry.get(parameterName), parameter);
+        query.where(predicate);
+        return em.createQuery(query).getResultList();
+    }
+    private static <T> void update(Class<T> objClass, Object parameter, Object newValue, String parameterName, Long id){
+        emfChecker();
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaUpdate<T> criteriaUpdate = cb.createCriteriaUpdate(objClass);
+        Root<T> root = criteriaUpdate.from(objClass);
+        criteriaUpdate.set(parameterName, newValue);
+        criteriaUpdate.where(cb.equal(root.get("id"), id)); // do sprawdzenia na pewno
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+            em.createQuery(criteriaUpdate).executeUpdate();
+            transaction.commit();
+        } catch (Exception e){
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw e;
+        } finally {
+            em.close();
+        }
+    }
 }
