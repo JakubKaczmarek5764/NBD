1f3a49db093bde4d30483d25165cbd11a2bd29ca
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2024-11-22 15:39:41 +0100

    connectionstring w osobnym pliku konfiguracyjnym

--- src/main/java/repositories/AbstractRedisRepository.java
+++ src/main/java/repositories/AbstractRedisRepository.java
@@ -5,11 +5,23 @@ import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPool;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class AbstractRedisRepository {
     private JedisPool jedisPool;
     public JedisPool initConnection(){
         if (jedisPool == null) {
-            jedisPool = new JedisPool("localhost", 6379);
+            try {
+                Properties props = new Properties();
+                props.load(new FileInputStream("redis.properties"));
+
+                JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().build();
+                jedisPool = new JedisPool(props.getProperty("redis.host"), Integer.parseInt(props.getProperty("redis.port")));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return jedisPool;
     }
