5b2c1247a50c4e65fe9211f585cea1b6705c27f2
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2024-11-23 13:54:34 +0100

    hihi

--- src/main/java/repositories/RedisLiteratureRepository.java
+++ src/main/java/repositories/RedisLiteratureRepository.java
@@ -25,6 +25,7 @@ public class RedisLiteratureRepository extends AbstractRedisRepository implement
 
     @Override
     public void create(Literature obj) {
+        mongoLiteratureRepository.create(obj);
         String id = obj.getLiteratureId().getId().toString();
         String literatureJson = jsonb.toJson(obj);
         jedisPool.jsonSet(hashPrefix + id, literatureJson);
@@ -32,31 +33,42 @@ public class RedisLiteratureRepository extends AbstractRedisRepository implement
 
     @Override
     public List<Literature> getAll() {
-        List<Literature> literatures = new ArrayList<>();
-        String cursor = "0";
-        do {
-            ScanResult<String> result = jedisPool.scan(cursor, new ScanParams().match(hashPrefix + "*"));
-            cursor = result.getCursor();
-            for (String literatureJson : result.getResult()) {
-                literatures.add(jsonb.fromJson(literatureJson, Literature.class));
-            }
-        } while (!cursor.equals("0"));
-        return literatures;
+        try {
+            List<Literature> literatures = new ArrayList<>();
+            String cursor = "0";
+            do {
+                ScanResult<String> result = jedisPool.scan(cursor, new ScanParams().match(hashPrefix + "*"));
+                cursor = result.getCursor();
+                for (String literatureJson : result.getResult()) {
+                    literatures.add(jsonb.fromJson(literatureJson, Literature.class));
+                }
+            } while (!cursor.equals("0"));
+            return literatures;
+        } catch (Exception e) { // to takie robocze chyba rozwiazanie Zaimplementuj metodę, która pobierze dane z bazy danych MongoDB w przypadku utraty połączenia z Redisem.
+            return mongoLiteratureRepository.getAll();
+        }
     }
 
     @Override
     public Literature getById(MongoUniqueId id) {
-        String literatureJson = jedisPool.get(hashPrefix + id.getId().toString());
-        return jsonb.fromJson(literatureJson, Literature.class);
+        try {
+            String literatureJson = jedisPool.get(hashPrefix + id.getId().toString());
+            return jsonb.fromJson(literatureJson, Literature.class);
+        } catch (Exception e) { // to tez
+            return mongoLiteratureRepository.getById(id);
+        }
+
     }
 
     @Override
     public void delete(Literature obj) {
+        mongoLiteratureRepository.delete(obj);
             jedisPool.del(hashPrefix + obj.getLiteratureId().getId().toString());
     }
 
     @Override
     public void update(Literature obj) {
+        mongoLiteratureRepository.update(obj);
         // ciekawe jak zrobic update
         String id = obj.getLiteratureId().getId().toString();
         String literatureJson = jsonb.toJson(obj);
@@ -66,12 +78,13 @@ public class RedisLiteratureRepository extends AbstractRedisRepository implement
 
     @Override
     public void emptyCollection() {
+        mongoLiteratureRepository.emptyCollection();
 //            jedisPool.del(hashPrefix + "*");
         jedisPool.flushAll();
     }
 
     @Override
     public MongoCollection<Literature> getLiteratureCollection() {
-        return null;
+        return mongoLiteratureRepository.getLiteratureCollection();
     }
 }
