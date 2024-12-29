97b1e15e6a4260ef713bd5cab74f35eabe851bb0
247731 Marszałek Piotr <247731@edu.p.lodz.pl>
2024-12-29 15:47:39 +0100

    abstract repository

---
+++ src/main/java/repositories/AbstractCassandraRepository.java
@@ -0,0 +1,28 @@
+package repositories;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.CqlSession;
+
+import java.net.InetSocketAddress;
+
+public class AbstractCassandraRepository implements AutoCloseable {
+    private static CqlSession session;
+
+    public void initSession() {
+        session = CqlSession.builder()
+                .addContactPoint(new InetSocketAddress("cassandra1", 9042))
+                .addContactPoint(new InetSocketAddress("cassandra2", 9043))
+                .withLocalDatacenter("dc1")
+                .withAuthCredentials("cassandra", "cassandrapassword")
+//                .withKeyspace(CqlIdentifier.fromCql("nbd"))
+                .build();
+    }
+    public static CqlSession getSession() {
+        return session;
+    }
+
+    @Override
+    public void close() throws Exception {
+        session.close();
+    }
+}
